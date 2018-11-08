31c568a553b4c3bee15842e1e2f7b9c2a9b5492e
shshelpdesk <44614903+shshelpdesk@users.noreply.github.com>
2018-11-08 08:30:32 -0800

    Create test site

---
+++ test/index.js
@@ -0,0 +1,22 @@
+$(document).ready(function() {
+	$.getJSON('test.json', function(data) {         
+		console.log(data+"please no");
+	});
+  $(".needpad").css("opacity", "0");
+  $("#maincontent").hide().show('slow', function() {
+    $(".needpad").fadeTo("slow", 1.0);
+  });
+});
+
+function showSolutions() {
+	$(".needpad").fadeTo("slow", 0.0, function() {
+		$("#maincontent").hide("slow");
+	});
+}
+
+function showMain() {
+	$(".needpad").fadeTo("slow", 0.0, function() {
+		$("#maincontent").hide("slow");
+	});
+}
+
