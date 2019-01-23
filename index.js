d8e370ac0d03d91668fa98d31f853a8c0b414321
shshelpdesk <44614903+shshelpdesk@users.noreply.github.com>
2019-01-23 09:41:49 -0800

    Pushed via push utility.

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
+
+
+
+var entHeight = 332
 var entryNum=0;
 var ip2=0;
 var canSwitch = true;
@@ -15,14 +19,13 @@ var newStr;
 
 
 $(document).ready(function() {
-	console.log("runred");
+	$("#mainpicture").height(Math.round(entHeight*0.55));
   	$(".needpad").css("opacity", "0");
   	$(".textin").css("opacity", "0");
   	$("#maincontent").hide().show('slow', function() {
   	$(".needpad").fadeTo("slow", 1.0);
  	});
 	$.getJSON('entries.json', function(data) {
-		console.log("run")
 		for(var i in data) {
 			newStr = skeleton;
 			newStr = newStr.replace("PLCHOLDTITLE", (data[i]["title"]));
@@ -30,7 +33,6 @@ $(document).ready(function() {
 			newStr = newStr.replace("PLCHOLDID", ("newsEnt"+entryNum.toString()));
 			entryNum += 1;
 			$("#allcontain").append(newStr);
-			console.log(newStr);
 		}
 	});
 
@@ -44,7 +46,7 @@ function showSolutions() {
 	$(".textin").css("opacity", "0");
 	$(".needpad").fadeTo("slow", 0.0, function() {
 		$("#maincontent").hide("slow", function() {
-			$("#allcontain").show("slow", function() {
+			$("#allcontain").slideDown("slow", function() {
 				$(".textin").fadeTo("slow", 1.0);
 			});
 		});
@@ -54,7 +56,7 @@ function showSolutions() {
 function showMain() {
 	$(".needpad").css("opacity", "0");
 	$(".textin").fadeTo("slow", 0.0, function() {
-		$("#allcontain").hide("slow", function(){
+		$("#allcontain").slideUp("slow", function(){
 			$("#maincontent").show("slow", function() {
 				$(".needpad").fadeTo("slow", 1.0);
 			});
